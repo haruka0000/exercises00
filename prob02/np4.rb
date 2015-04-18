bd3be0eb189ae67a58ae5ab6689479638b85dd8b
haruka0000 <1124226@st.anan-nct.ac.jp>
2015-04-18 17:18:38 +0900

    put on comment

--- prob02/np4.rb
+++ prob02/np4.rb
@@ -1,16 +1,16 @@
 #! ruby -Ku
 require "kconv"
 
-	size = 4
-	div = size**(1/2.0)
-	num = [1,2,3,4]
+	size = 4 #sudoku size
+	div = size**(1/2.0) # to divide area 4
+	num = [1,2,3,4] 
 	vertical = Array.new(size)
         vertical[0] = [nil,nil,3,nil]
         vertical[1] = [nil,nil,2,nil]
         vertical[2] = [nil,nil,nil,nil]
         vertical[3] = [3,4,nil,nil]
 	
-	def printVer(vertical)
+	def printVer(vertical) #print sudoku
 		for v in vertical do
 			print("|")
                 	for h in 0..v.size-1 do
@@ -26,33 +26,35 @@ require "kconv"
 	end
 	printVer(vertical)
 ###########################################################	
-	def check(ver,ve,ho,d,number)
+	def check(ver,ve,ho,d,number) #get array, v-number,h-number,area-div,number
 		c=0
-		for l in ver[ve] do
+		for l in ver[ve] do #check vertical
 			if l == number then
 				c = false
 				return c
+				break
 			end
 		end
 		
-		for i in 0..ver.size-1 do
+		for i in 0..ver.size-1 do #check horizontal
 			if ver[i][ho]==number then
 				c = false
 				return c
+				break
 			end
 		end
 
 		asize = d*d
 		judge = Array.new(asize)
-#		for j in judge do
+#		for j in judge do #area reset
 #			j = Array.new(d*d)
 #			for i in j do
 #				i=nil
 #			end
 #		end
-		areanum=ve/d*2+ho/d
+		areanum=ve/d*2+ho/d #area numbering
 
-	#	for j in judge[areanum] do
+	#	for j in judge[areanum] do #check area
 	#			if j==vertical[ve][ho] then
 	#				c=false
 	#			end
@@ -62,7 +64,7 @@ require "kconv"
 
 	
 	k=0
-	for v in 0..vertical.size-1 do
+	for v in 0..vertical.size-1 do #enterring number
                 for h in 0..vertical[v].size-1 do
 			while true
 				if check(vertical,v,h,div,num[k])
@@ -72,7 +74,7 @@ require "kconv"
 					k=k+1
 				end
 
-				if k>size-1 then
+				if k>size-1 then #roop in num 1,2,3,4,1,2,3...
 					k=0
 				end
 			end
@@ -81,4 +83,4 @@ require "kconv"
         end
 	print("end\n")
 ###########################################################
-	printVer(vertical)
+	printVer(vertical) #print sudoku
